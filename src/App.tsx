7d98feb4e4fced09e29d05fc19cb2f32ce133517
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-04-17 11:13:27 +0200

    header uselocation fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Groups from './pages/Groups';
+import Users from './pages/Users';
+import Assignments from './pages/Assignments';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/groups" element={<Groups />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="/assignments" element={<Assignments />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import darkLogo from '../assets/Saltwhite.svg';
 
 export interface HeaderProps {
@@ -8,8 +9,13 @@ export interface HeaderProps {
 const baseHeader = 'text-white bg-neutral-700 mb-4 flex justify-center';
 
 export const Header = ({ items }: HeaderProps) => {
+  let location = useLocation().pathname.toLowerCase();
+  console.log('url', location);
+
   const ulClass = 'flex my-2';
   const liClass = 'font-poppins mx-2 bottom-0 text-base';
+  const liClassSelected =
+    'font-poppins mx-2 bottom-0 text-base border-b-[2px] border-pink-600';
 
   return (
     <header className={baseHeader}>
@@ -21,7 +27,10 @@ export const Header = ({ items }: HeaderProps) => {
           <nav className="flex">
             <ul className={ulClass}>
               {items.map((item) => (
-                <li className={liClass} key={item.href}>
+                <li
+                  className={location === item.href ? liClassSelected : liClass}
+                  key={item.href}
+                >
                   <a href={item.href}>{item.label}</a>
                 </li>
               ))}
