ed97fb4399298016653342d5fefa6f1f896038f4
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-05-08 13:47:41 +0200

    updated login file

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
 import { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { AppContext } from '../AppContext';
-import { useAppSelector, useAppDispatch } from '../hooks/reduxHooks';
-import { loginReducer } from '../slices/userSlice';
 import { ContextType } from '../types';
-=======
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useGoogleLogin } from '@react-oauth/google';
-// import { ContextType } from '../types';
-// import { AppContext } from '../AppContext';
->>>>>>> 5374dde3bdfba8bec4f2e7a0c91ec262b9a7bc1c
 import Title from '../components/Title';
 import { Button } from '../components/Button';
 import lightLogo from '../assets/Saltblack.svg';
 
 const Login = () => {
-<<<<<<< HEAD
   const { user, setUser, profile, setProfile } = useContext(
     AppContext
   ) as ContextType;
@@ -29,13 +18,6 @@ const Login = () => {
   }>();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-=======
-  // const { user, setUser } = useContext(AppContext) as ContextType;
-  const [userToken, setUserToken] = useState();
-  const [profile, setProfile] = useState<IProfile>({} as IProfile);
-
->>>>>>> 5374dde3bdfba8bec4f2e7a0c91ec262b9a7bc1c
   const login = useGoogleLogin({
     onSuccess: (codeResponse) => setUserGoogleToken(codeResponse)
   });
@@ -68,7 +50,6 @@ const Login = () => {
           fullName: profile.name,
           imageUrl: profile.picture
         })
-<<<<<<< HEAD
         .then((res) => {
           setUser(res.data);
           Cookies.set('userId', res.data.id, {
@@ -77,22 +58,8 @@ const Login = () => {
           Cookies.set('token', res.data.token, {
             expires: 30
           });
-          navigate('/home', { state: { id: res.data.id } });
+          navigate('/home');
         })
-=======
-        // .then(() => {
-        //   axios.put(
-        //     'https://project-salty-backend.azurewebsites.net/Users/login',
-        //     {
-        //       email: profile.email,
-        //       fullName: profile.name,
-        //       imageURL: profile.picture,
-        //       googleId: profile.googleId
-        //     }
-        //   );
-        // })
-        // .then((res) => console.log(res))
->>>>>>> 5374dde3bdfba8bec4f2e7a0c91ec262b9a7bc1c
         .catch((err) => console.log(err));
     }
   }, [profile]);
