735446815c251ce118b9f28828c068b433773674
Farhana K <farhanak@Farhanas-Air.home>
2023-05-10 17:02:37 +0200

    routes protected

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {  useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
@@ -15,52 +15,77 @@ import AddGroup from './pages/AddGroup';
 import UpdateUser from './pages/UpdateUser';
 import UpdateAssignment from './pages/UpdateAssignment';
 import { ContextType } from '../src/types';
+import secureLocalStorage from 'react-secure-storage';
 
 import { AppContext } from './AppContext';
-const PrivateRoute = ({pageComponent} : any) => {
+const PrivateRoute = ({ pageComponent }: any) => {
   const { user } = useContext(AppContext) as ContextType;
   const navigate = useNavigate();
+  let localUserId = secureLocalStorage.getItem('id');
 
   useEffect(() => {
-    console.log(user.id)
-    if(!user.id ) {
-      navigate('/')
+    console.log(user.id);
+    if (!user.id && !localUserId) {
+      navigate('/');
     }
-  }, )
+  });
   return pageComponent;
-}
+};
 function App() {
-
   return (
     <Routes>
       {/* public routes  */}
       <Route path="/" element={<Login />} />
-      {/* All private routes */}
-      <Route
-            path="/home"
-            element={<PrivateRoute pageComponent={<Home />} />}
-          />
-        
-        <Route
-            path="/groups"
-            element={<PrivateRoute pageComponent={<Groups />} />}
-          />
-      {/* <Route path="/home" element={<Home />} /> */}
 
-      <Route path="/groups" element={<Groups />} />
-      <Route path="/groups/:groupId" element={<Group />} />
-      <Route path="/groups/new" element={<AddGroup />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/users/:userId" element={<User />} />
-      <Route path="/users/:userId/update" element={<UpdateUser />} />
-      <Route path="/users/new" element={<AddUser />} />
-      <Route path="/assignments/:assignmentId" element={<Assignment />} />
+      {/* All private routes */}
+      <Route path="/home" element={<PrivateRoute pageComponent={<Home />} />} />
+
+      <Route
+        path="/groups"
+        element={<PrivateRoute pageComponent={<Groups />} />}
+      />
+
+      <Route
+        path="/groups/:groupId"
+        element={<PrivateRoute pageComponent={<Group />} />}
+      />
+
+      <Route
+        path="/groups/new"
+        element={<PrivateRoute pageComponent={<AddGroup />} />}
+      />
+      <Route
+        path="/users"
+        element={<PrivateRoute pageComponent={<Users />} />}
+      />
+      <Route
+        path="/users/:userId"
+        element={<PrivateRoute pageComponent={<User />} />}
+      />
+      <Route
+        path="/users/:userId/update"
+        element={<PrivateRoute pageComponent={<UpdateUser />} />}
+      />
+      <Route
+        path="/users/new"
+        element={<PrivateRoute pageComponent={<AddUser />} />}
+      />
+      <Route
+        path="/assignments/:assignmentId"
+        element={<PrivateRoute pageComponent={<Assignment />} />}
+      />
       <Route
         path="/assignments/:assignmentId/update"
-        element={<UpdateAssignment />}
+        element={<PrivateRoute pageComponent={<UpdateAssignment />} />}
       />
-      <Route path="/assignments/new" element={<AddAssignment />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route
+        path="/assignments/new"
+        element={<PrivateRoute pageComponent={<AddAssignment />} />}
+      />
+      <Route
+        path="/profile"
+        element={<PrivateRoute pageComponent={<Profile />} />}
+      />
     </Routes>
   );
 }
