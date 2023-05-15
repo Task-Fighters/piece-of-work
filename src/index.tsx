c79d54eb7b668d35159c082cab93f30519eab078
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-05-15 12:15:20 +0200

    removed redux artifacts

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import App from './App';
 import { AppProvider } from './AppContext';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import {store} from './store'
-import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,14 +12,12 @@ const root = ReactDOM.createRoot(
 root.render(
   // @ts-ignore
   <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-    {/* <React.StrictMode> */}
-    <Provider store={store} >
+    <React.StrictMode>
       <BrowserRouter>
         <AppProvider>
           <App />
         </AppProvider>
       </BrowserRouter>
-    </Provider>
-    {/* </React.StrictMode> */}
+    </React.StrictMode>
   </GoogleOAuthProvider>
 );
